5f92462052ece87b4e8a34ce599d0a579ef2189b
Hiram Muñoz <hirammunozr@icloud.com>
2017-02-03 15:27:44 -0600

    1.0

--- ball.py
+++ ball.py
@@ -1,4 +1,4 @@
-import time
+import time, sys
 from random import randint
 responses = []
 with open('responses.txt', 'r') as f:       
@@ -6,6 +6,18 @@ with open('responses.txt', 'r') as f:
       response = str(line)
       responses.append(response)  
 rnumber = len(responses)
-input("What is your question?")
+input("What is your question?\n")
 rn = randint(1,rnumber)
-print(responses[rn])
+print("\rThinking..", end='')
+for num in range(0,20):
+    time.sleep(0.2)
+    if num % 4 == 0:
+        print('\rThinking.   ', end='')
+    elif num % 4 == 1:
+        print('\rThinking..   ', end='')
+    elif num % 4 == 2:
+        print('\rThinking... ', end='')
+    else:
+        print('\rThinking....', end='')
+
+print("\n" + responses[rn])
